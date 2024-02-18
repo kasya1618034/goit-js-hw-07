b6aaccfe98d6b244382a8066e7d25f3455977d6b
Katarzyna Filas <katarzyna.filas@gmail.com>
2024-02-18 23:55:14 +0100

    Update task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+const ArrangedGalleryItems = galleryItems.map((item) =>`
+    <li><a class='gallery__item' href='${item.original}'><img class='gallery__image' src='${item.preview}'
+    alt='${item.description}'></a></li>
+`);
+gallery.innerHTML = ArrangedGalleryItems.join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionsDelay: 250, 
+});
+
 console.log(galleryItems);
